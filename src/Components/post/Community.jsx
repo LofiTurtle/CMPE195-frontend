8614273d405b4bb689009172096aa12a57e78868
LofiTurtle <46658877+LofiTurtle@users.noreply.github.com>
2024-10-18 09:50:26 -0700

    Add community follow button

--- src/Components/post/Community.jsx
+++ src/Components/post/Community.jsx
@@ -8,6 +8,7 @@ const Community = () => {
   const { communityId } = useParams();
 
   const [community, setCommunity] = useState({name: 'Loading...', num_users: 0});
+  const [isFollowing, setIsFollowing] = useState(null);
 
   useEffect(() => {
     const getCommunity = async () => {
@@ -15,14 +16,31 @@ const Community = () => {
       setCommunity(community);
     }
 
-    getCommunity();
-  }, [])
+    const getIsFollowing = async () => {
+      const {user: currentUser} = await api.getMe();
+      const { users } = await api.getCommunityUsers(communityId);
+      setIsFollowing(users.map((x) => x.id).includes(currentUser.id));
+    }
 
+    getCommunity();
+    getIsFollowing();
+  }, [communityId])
+
+  const toggleFollowCommunity = async () => {
+    if (isFollowing) {
+      await api.unfollowCommunity(community.id);
+      setIsFollowing(false);
+    } else {
+      await api.followCommunity(community.id);
+      setIsFollowing(true);
+    }
+  }
 
   return (
     <div>
       <h1>{community.name}</h1>
-      {/* <button>Follow Community (not yet implemented)</button> */}
+      <button onClick={toggleFollowCommunity}>{isFollowing ? 'Unfollow' : 'Follow'} Community</button>
+      <br />
       <Link to={`/create-post`}>Create New Post</Link>
       <p>{community.num_users} members</p>
       <PostList communityId={communityId}></PostList>
