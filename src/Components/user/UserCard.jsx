9db22b8c67d5c35b9183d343323e1efd7765ea1d
LofiTurtle <46658877+LofiTurtle@users.noreply.github.com>
2024-11-01 10:46:28 -0700

    Remove log statement

--- src/Components/user/UserCard.jsx
+++ src/Components/user/UserCard.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 
 const UserCard = ({ user }) => {
   const navigate = useNavigate();
-  console.log(user);
 
   return (
     <p onClick={() => navigate(`/users/${user.id}`)} style={{cursor: 'pointer'}}>{user.username}</p>
