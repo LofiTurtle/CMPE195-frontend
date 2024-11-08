f8d8b0435170426a1e4b9503e096049a58efd65c
LofiTurtle <46658877+LofiTurtle@users.noreply.github.com>
2024-11-08 11:11:57 -0800

    Improve user lists

--- src/Components/user/UserCard.jsx
+++ src/Components/user/UserCard.jsx
@@ -4,8 +4,27 @@ import { useNavigate } from 'react-router-dom';
 const UserCard = ({ user }) => {
   const navigate = useNavigate();
 
+  const handleSelect = () => {
+    navigate(`/users/${user.id}`);
+  }
+
   return (
-    <p onClick={() => navigate(`/users/${user.id}`)} style={{cursor: 'pointer'}}>{user.username}</p>
+    <button
+      onClick={() => handleSelect()}
+      className="w-full p-2 text-left hover:bg-gray-100"
+    >
+      <div className="flex gap-2">
+          <img
+            src={`/api/users/${user.id}/profile-picture`}
+            alt=""
+            className="w-10 h-10 object-cover rounded flex-shrink-0"
+          />
+        <div className="min-w-0 flex-1">
+          <div className="font-medium truncate">{user.username}</div>
+          <div className="text-sm text-gray-600 truncate">{user.profile.bio}</div>
+        </div>
+      </div>
+    </button>
   )
 }
 
--- src/Components/user/UserCardList.jsx
+++ src/Components/user/UserCardList.jsx
@@ -3,9 +3,14 @@ import UserCard from './UserCard';
 
 const UserCardlist = ({ users }) => {
   return (
-    <ul>
+    <ul
+      className="border rounded w-96 "
+    >
       {users.map(user => (
-        <li key={user.id}><UserCard user={user}/></li>
+        <li
+          key={user.id}
+          className={"border-b last:border-b-0"}
+        ><UserCard user={user}/></li>
       ))}
     </ul>
   );
