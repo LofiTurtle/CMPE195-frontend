d7b2f5aabde2e09d1fccc77782d57c79bf341ecb
LofiTurtle <46658877+LofiTurtle@users.noreply.github.com>
2024-11-08 08:54:48 -0800

    Remove unused imports

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { FaUserCircle } from 'react-icons/fa';
-import { IoMdNotificationsOutline } from 'react-icons/io';
+import {Link, useNavigate} from 'react-router-dom';
 import {useDispatch, useSelector} from "react-redux";
 
 
@@ -11,8 +9,7 @@ import {fetchUser} from "../slices/userSlice.js";
 
 export const Navbar = () => {
   const [accountMenuOpen, setAccountMenuOpen] = useState(false);
-  const [notificationMenuOpen, setNotificationMenuOpen] = useState(false);
-  const { userId, username, status, error } = useSelector((state) => state.user);
+  const {userId} = useSelector((state) => state.user);
 
   const navigate = useNavigate(); // To navigate after logout
   const dispatch = useDispatch();
@@ -22,11 +19,7 @@ export const Navbar = () => {
   useEffect(() => {
     // Handle clicking outside the search type dropdown
     const handleClickOutside = (event) => {
-      if (
-        accountMenuRef.current &&
-        !accountMenuRef.current.contains(event.target) &&
-        accountMenuOpen
-      ) {
+      if (accountMenuRef.current && !accountMenuRef.current.contains(event.target) && accountMenuOpen) {
         setAccountMenuOpen(false);
       }
     };
@@ -45,7 +38,7 @@ export const Navbar = () => {
   }, [dispatch]);
 
   const logout = async () => {
-    fetch('/api/logout', { method: 'POST' })
+    fetch('/api/logout', {method: 'POST'})
       .then(response => {
         if (response.status !== 200) {
           throw new Error();
@@ -58,53 +51,49 @@ export const Navbar = () => {
       .catch(() => console.log('Error logging out.'));
   };
 
-  return (
-    <nav>
-      <Link to='/dashboard' className='Title'>Game Sphere</Link>
-      
-      
-      <div className='search-bar'>
-        <SearchBox />
-      </div>
+  return (<nav>
+    <Link to='/dashboard' className='Title'>Game Sphere</Link>
 
-      <ul className='navbar-items'>
-       
-      <li><button onClick={() => navigate('/create-post')}>New Post</button></li>
 
-        {/* Account Section */}
-        <li>
-          <div className='menu-container' ref={accountMenuRef}>
-            <div
-              className='menu-trigger'
-              
-              onClick={() => { setAccountMenuOpen(!accountMenuOpen); }}
-            >
-              <img src={`/api/users/${userId}/profile-picture`} alt="" className={'w-9'}/>
-            </div>
-            <div className={`dropdown-menu ${accountMenuOpen ? 'active' : 'inactive'}`}>
-              <ul>
-                <DropdownItem text={"My Profile"} to="" />
-                <DropdownItem text={"Settings"} to="/settings" />
-                <DropdownItem text={"Logout"} onClick={logout} />
-              </ul>
-            </div>
+    <div className='search-bar'>
+      <SearchBox/>
+    </div>
+
+    <ul className='navbar-items'>
+
+      <li>
+        <button onClick={() => navigate('/create-post')}>New Post</button>
+      </li>
+
+      {/* Account Section */}
+      <li>
+        <div className='menu-container' ref={accountMenuRef}>
+          <div
+            className='menu-trigger'
+
+            onClick={() => {
+              setAccountMenuOpen(!accountMenuOpen);
+            }}
+          >
+            <img src={`/api/users/${userId}/profile-picture`} alt="" className={'w-9'}/>
           </div>
-        </li>
-      </ul>
-    </nav>
-  );
+          <div className={`dropdown-menu ${accountMenuOpen ? 'active' : 'inactive'}`}>
+            <ul>
+              <DropdownItem text={"My Profile"} to=""/>
+              <DropdownItem text={"Settings"} to="/settings"/>
+              <DropdownItem text={"Logout"} onClick={logout}/>
+            </ul>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </nav>);
 };
 
 function DropdownItem(props) {
-  return (
-    <li className='dropdown-item'>
-      {props.to ? (
-        <Link to={props.to}>{props.text}</Link>
-      ) : (
-        <a onClick={props.onClick}>{props.text}</a>
-      )}
-    </li>
-  );
+  return (<li className='dropdown-item'>
+    {props.to ? (<Link to={props.to}>{props.text}</Link>) : (<a onClick={props.onClick}>{props.text}</a>)}
+  </li>);
 }
 
 export default Navbar;
