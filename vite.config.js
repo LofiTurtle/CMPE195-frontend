fca2d29d179ac7feaa257b76eaff41c95b893dc6
LofiTurtle <46658877+LofiTurtle@users.noreply.github.com>
2024-11-18 00:08:36 -0800

    Enable test coverage reports

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import istanbul from 'vite-plugin-istanbul'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    istanbul({
+      exclude: ['node_modules', 'cypress/'],
+      extensions: ['.js', '.jsx'],
+      requireEnv: false,
+      forceBuildInstrument: true
+    })
+  ],
   server: {
     host: '127.0.0.1',
     proxy: {
-      '/api': 'http://127.0.0.1:5000/',
-      // '/auth': 'http://localhost:5000'
+      '/api': 'http://127.0.0.1:5000/'
     }
+  },
+  build: {
+    sourcemap: true
   }
 })
